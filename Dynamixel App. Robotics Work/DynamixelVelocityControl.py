f2c9153ef6c81257a07fa16af8388b5f880133a7
VictorJB24 <victorjb2015@gmail.com>
2021-05-04 16:34:31 -0700

    Added boot up function, added shutdown function, started on turn calculations file

--- Dynamixel App. Robotics Work/DynamixelVelocityControl.py
+++ Dynamixel App. Robotics Work/DynamixelVelocityControl.py
@@ -1,5 +1,5 @@
-#*******************************************************************************
-#***********************     Read and Write Example      ***********************
+# *******************************************************************************
+# ***********************     Read and Write Example      ***********************
 #  Required Environment to run this example :
 #    - Protocol 2.0 supported DYNAMIXEL(X, P, PRO/PRO(A), MX 2.0 series)
 #    - DYNAMIXEL Starter Set (U2D2, U2D2 PHB, 12V SMPS)
@@ -16,12 +16,17 @@ import os
 
 if os.name == 'nt':
     import msvcrt
+
+
     def getch():
         return msvcrt.getch().decode()
 else:
     import sys, tty, termios
+
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
+
+
     def getch():
         try:
             tty.setraw(sys.stdin.fileno())
@@ -30,11 +35,21 @@ else:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
-from dynamixel_sdk import * # Uses Dynamixel SDK library
+from dynamixel_sdk import *  # Uses Dynamixel SDK library
 
-#********* DYNAMIXEL Model definition *********
-#***** (Use only one definition at a time) *****
-MY_DXL = 'X_SERIES'       # X330 (5.0 V recommended), X430, X540, 2X430
+# ********* DYNAMIXEL Model definition *********
+# ***** (Use only one definition at a time) *****
+MY_DXL = 'X_SERIES'  # X330 (5.0 V recommended), X430, X540, 2X430
+
+
+def getIDs():
+    ids = []
+    numOfIDs = input("How many ID's to use: ")
+    for i in range(int(numOfIDs)):
+        singleID = int(input(f"ID #{i + 1}: "))
+        ids.append(singleID)
+    return ids
+
 
 MODE_SET_ADDR = 11
 VEL_MODE = 1
@@ -43,36 +58,34 @@ EXT_POS = 4
 PWM_CONTR = 16
 
 GOAL_VEL_ADDR = 104
-GOAL_VEL = 200
-
+GOAL_VEL = 265
 
 # Control table address
 if MY_DXL == 'X_SERIES' or MY_DXL == 'MX_SERIES':
-    ADDR_TORQUE_ENABLE          = 64
-    ADDR_GOAL_POSITION          = 116
-    ADDR_PRESENT_POSITION       = 132
-    DXL_MINIMUM_POSITION_VALUE  = 0         # Refer to the Minimum Position Limit of product eManual
-    DXL_MAXIMUM_POSITION_VALUE  = 4095      # Refer to the Maximum Position Limit of product eManual
-    BAUDRATE                    = 57600
+    ADDR_TORQUE_ENABLE = 64
+    ADDR_GOAL_POSITION = 116
+    ADDR_PRESENT_POSITION = 132
+    DXL_MINIMUM_POSITION_VALUE = 0  # Refer to the Minimum Position Limit of product eManual
+    DXL_MAXIMUM_POSITION_VALUE = 4095  # Refer to the Maximum Position Limit of product eManual
+    BAUDRATE = 57600
 
 # DYNAMIXEL Protocol Version (1.0 / 2.0)
 # https://emanual.robotis.com/docs/en/dxl/protocol2/
-PROTOCOL_VERSION            = 2.0
+PROTOCOL_VERSION = 2.0
 
 # Factory default ID of all DYNAMIXEL is 1
-DXL_ID                      = 4
+DXL_IDs = getIDs()
 
 # Use the actual port assigned to the U2D2.
 # ex) Windows: "COM*", Linux: "/dev/ttyUSB*", Mac: "/dev/tty.usbserial-*"
-DEVICENAME                  = '/dev/tty.usbserial-FT3WHPY9' # Change port depending on motor controller Ex. 'COM5' or '/dev/tty.usbserial-FT3WHPY9'
+DEVICENAME = 'COM5'  # Change port depending on motor controller Ex. 'COM5' or '/dev/tty.usbserial-FT3WHPY9'
 
-TORQUE_ENABLE               = 1     # Value for enabling the torque
-TORQUE_DISABLE              = 0     # Value for disabling the torque
-DXL_MOVING_STATUS_THRESHOLD = 20    # Dynamixel moving status threshold
+TORQUE_ENABLE = 1  # Value for enabling the torque
+TORQUE_DISABLE = 0  # Value for disabling the torque
+DXL_MOVING_STATUS_THRESHOLD = 20  # Dynamixel moving status threshold
 
 index = 0
-dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]         # Goal position
-
+dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]  # Goal position
 
 # Initialize PortHandler instance
 # Set the port path
@@ -85,8 +98,10 @@ portHandler = PortHandler(DEVICENAME)
 packetHandler = PacketHandler(PROTOCOL_VERSION)
 
 def setMode():
-    packetHandler.write1ByteTxRx(portHandler, DXL_ID, MODE_SET_ADDR, VEL_MODE)
+    for element in DXL_IDs:
+        packetHandler.write1ByteTxRx(portHandler, element, MODE_SET_ADDR, VEL_MODE)
     print("Velocity mode set")
+
 
 # Open port
 if portHandler.openPort():
@@ -96,7 +111,6 @@ else:
     print("Press any key to terminate...")
     getch()
     quit()
-
 
 # Set port baudrate
 if portHandler.setBaudRate(BAUDRATE):
@@ -110,50 +124,43 @@ else:
 setMode()
 
 # Enable Dynamixel Torque
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
-if dxl_comm_result != COMM_SUCCESS:
-    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-elif dxl_error != 0:
-    print("%s" % packetHandler.getRxPacketError(dxl_error))
-else:
-    print("Dynamixel has been successfully connected")
+for element in DXL_IDs:
+    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, element, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+    else:
+        print(f"Dynamixel motor with ID #{element} has been successfully connected")
 
-
-while 1:
+while True:
     print("Press any key to continue! (or press ESC to quit!)")
     if getch() == chr(0x1b):
-        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, GOAL_VEL_ADDR, 0)
+        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_IDs[0], GOAL_VEL_ADDR, 0)
         break
 
     # Write goal position
-    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, GOAL_VEL_ADDR, GOAL_VEL)
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_IDs[0], GOAL_VEL_ADDR, GOAL_VEL)
     if dxl_comm_result != COMM_SUCCESS:
         print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
         print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-    while 1:
+    for i in range(1):
         # Read present position
-        dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, DXL_ID, ADDR_PRESENT_POSITION)
+        dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, DXL_IDs[0],
+                                                                                       ADDR_PRESENT_POSITION)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
             print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-        print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL_ID, dxl_goal_position[index], dxl_present_position))
-
         if not abs(dxl_goal_position[index] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD:
             break
-
-##    # Change goal position
-##    if index == 0:
-##        index = 1
-##    else:
-##        index = 0
-
+    print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL_IDs[0], dxl_goal_position[index], dxl_present_position))
 
 # Disable Dynamixel Torque
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_IDs[0], ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
 if dxl_comm_result != COMM_SUCCESS:
     print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
 elif dxl_error != 0:
---
+++ Dynamixel App. Robotics Work/turns.py
@@ -0,0 +1,17 @@
+from dynamixel_sdk import *  # Uses Dynamixel SDK library
+import os
+
+
+def pointTurn(sensorVals):
+    #  write function to calculate motor speeds for point turn based on sensor values
+    return ["Motor speed one", "motor speed two", "motor speed three", "motor speed four"]
+
+
+def pointTurn(sensorVals):
+    #  write function to calculate motor speeds for corner turn based on sensor values
+    return ["Motor speed one", "motor speed two", "motor speed three", "motor speed four"]
+
+
+def pointTurn(sensorVals):
+    #  write function to calculate motor speeds for wide turn based on sensor values
+    return ["Motor speed one", "motor speed two", "motor speed three", "motor speed four"]
